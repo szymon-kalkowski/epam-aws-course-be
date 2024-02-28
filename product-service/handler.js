5998a6cbe96cd36bf8530b537b8867de92f199d6
Szymon Kalkowski <szymon.kalkowski@wp.pl>
2024-02-28 20:26:37 +0100

    task-4

--- product-service/handler.js
+++ product-service/handler.js
@@ -1,25 +1,86 @@
 "use strict";
 
-const products = require("./data.js");
+const AWS = require("aws-sdk");
+const { v4: uuidv4 } = require("uuid");
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.getProductsList = async (event) => {
+  const products = await dynamoDb.scan({ TableName: "products" }).promise();
+  const stocks = await dynamoDb.scan({ TableName: "stocks" }).promise();
+
+  products.Items.forEach((product) => {
+    const stock = stocks.Items.find((stock) => stock.product_id === product.id);
+    product.count = stock.count;
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify(products),
+    body: JSON.stringify(products.Items),
   };
 };
 
 module.exports.getProductsById = async (event) => {
   const { productId } = event.pathParameters;
-  const product = products.find((product) => product.id === productId);
+
+  const product = await dynamoDb
+    .query({
+      TableName: "products",
+      KeyConditionExpression: "id = :id",
+      ExpressionAttributeValues: {
+        ":id": productId,
+      },
+    })
+    .promise();
+
   if (!product) {
     return {
       statusCode: 404,
       body: JSON.stringify({ message: "Product not found" }),
     };
   }
+
+  const stock = await dynamoDb
+    .query({
+      TableName: "stocks",
+      KeyConditionExpression: "product_id = :id",
+      ExpressionAttributeValues: {
+        ":id": productId,
+      },
+    })
+    .promise();
+
+  product.Items[0].count = stock.Items[0].count;
+
   return {
     statusCode: 200,
-    body: JSON.stringify(product),
+    body: JSON.stringify(product.Items[0]),
+  };
+};
+
+module.exports.createProduct = async (event) => {
+  const { title, description, price, count } = JSON.parse(event.body);
+
+  const product = {
+    id: uuidv4(),
+    title,
+    description,
+    price,
+  };
+
+  const stock = {
+    product_id: product.id,
+    count,
+  };
+
+  await dynamoDb.put({ TableName: "products", Item: product }).promise();
+  await dynamoDb.put({ TableName: "stocks", Item: stock }).promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      product,
+      stock,
+    }),
   };
 };
